ad7ff83e2359eef5fe0e56e239df7887116195b9
Rychillie <rychillie@hotmail.com>
2022-12-15 21:44:28 -0300

    👌 IMPROVE: Setup supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
-import type { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Session, SessionContextProvider } from "@supabase/auth-helpers-react";
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
+}>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
